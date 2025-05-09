eaa0cf5ab6c31e645f12aebcae06865e50bdd15e
Kasun Sagara <156450587+kasunsagara@users.noreply.github.com>
2025-05-10 01:34:06 +0530

    supabase is working fine

--- src/utils/mediaUpload.js
+++ src/utils/mediaUpload.js
@@ -1,8 +1,8 @@
 import { createClient } from "@supabase/supabase-js";
 
-const key = `eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImR2bm1ldXlhd25qcXJya3RiYnNoIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzU1NjI2MDksImV4cCI6MjA1MTEzODYwOX0.mfqpqLlXS4UgtlkZnvc36N5elE4tNVxo5QkyZC1eBUk`;
+const key = `eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImliZW9hYmdxd2hvd3Brb2d6bWViIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDY4MTM3NzMsImV4cCI6MjA2MjM4OTc3M30.rNCgLJLbvEbhvWLdtwYgWXDo-yRLc0ifPemPw-HEPvM`;
 
-const url = "https://dvnmeuyawnjqrrktbbsh.supabase.co";
+const url = "https://ibeoabgqwhowpkogzmeb.supabase.co";
 
 const supabase = createClient(url, key);
 
@@ -18,11 +18,11 @@ export default function uploadMediaToSupabase(file) {
 
     fileName = timestamp +file.name+ "." + extension;
 
-    supabase.storage.from("images").upload(fileName, file, {
+    supabase.storage.from("projectimages").upload(fileName, file, {
       cacheControl: "3600",
       upsert: false,
     }).then(()=>{
-      const publicUrl = supabase.storage.from("images").getPublicUrl(fileName).data.publicUrl;
+      const publicUrl = supabase.storage.from("projectimages").getPublicUrl(fileName).data.publicUrl;
       resolve(publicUrl);
     }).catch((err)=>{
       reject(err);
