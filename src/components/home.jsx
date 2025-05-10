c4d98f5aaef92bfb279b1f8c040908e759f74ba8
Kasun Sagara <156450587+kasunsagara@users.noreply.github.com>
2025-05-10 15:57:15 +0530

    small change

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -1,6 +1,5 @@
 import { useState, useEffect } from "react";
 import { FaFacebookF, FaLinkedinIn, FaGithub, FaWhatsapp } from "react-icons/fa";
-import { toast } from 'react-hot-toast';
 
 export default function Home() {
   const roles = [
@@ -29,7 +28,6 @@ export default function Home() {
       id="home"
       className="flex items-center justify-center min-h-screen bg-primary px-4 mt-16"
     >
-
       <div className="flex flex-col md:flex-row items-center justify-between max-w-6xl w-full gap-8 text-center md:text-left -mt-20">
         {/* Text content */}
         <div className="max-w-lg z-16">
@@ -87,11 +85,10 @@ export default function Home() {
             </a>
           </div>
 
-          {/* Download CV Button with Toast */}
+          {/* Download CV Button */}
           <a
             href="/Kasun_Sagara_CV.pdf" // Place the PDF in your public folder
             download
-            onClick={() => toast.success("CV download started!")}
             className="inline-block py-2 px-6 text-lg font-semibold bg-accent text-black rounded-lg shadow-lg hover:bg-gray-700 hover:text-white transition duration-300 z-10 relative"
           >
             Download CV
