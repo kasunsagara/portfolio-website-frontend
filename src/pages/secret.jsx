1d5c82b6a5dc75fbfa49703480df3a991888f847
Kasun Sagara <kasunsagara689@gmail.com>
2025-11-23 01:12:16 +0530

    color change

--- src/pages/secret.jsx
+++ src/pages/secret.jsx
@@ -92,7 +92,7 @@ const cardVariants = {
 
 export default function Secret() {
   return (
-    <div className="min-h-screen bg-gradient-to-br from-gray-900 via-gray-800 to-gray-900 py-8">
+    <div className="min-h-screen bg-gradient-to-br from-gray-900 to-black py-8">
       <motion.div
         variants={containerVariants}
         initial="hidden"
